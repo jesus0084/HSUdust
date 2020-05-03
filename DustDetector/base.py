6b788fff6f6d76c4d29d9e020d3bc70a0d131021
jesus0084 <jesus0084@naver.com>
2020-05-03 22:38:22 +0900

    UI Updated
    More Fine UI

--- DustDetector/base.py
+++ DustDetector/base.py
@@ -8,19 +8,20 @@ from updateAirKorea import updateAirKRData
 
 win=Tk()
 
-
 def exitProgram():
 	print("User Aborted Program")
 	win.quit()
+	return 0
 	
 def updateData():
 	indoorDataRaw = updateArdData()
 	outdoorDataRaw = updateAirKRData()
 	indoorDataParse = indoorDataRaw.decode().split(',')
-	indoorDustData = "PM 2.5 GRIMM : "+indoorDataParse[1] + ", PM 10 GRIMM : " + indoorDataParse[2]
-	indoorTHData = "Humid : " + indoorDataParse[5] +"%, Temp : "+indoorDataParse[6]+"C"
-	indoorDustShow.configure(text = indoorDustData)
-	indoorTHShow.configure(text = indoorTHData)
+		
+	indoorDustShow.configure(text = "미세먼지 : "+indoorDataParse[3]+"㎍/㎡")
+	indoorFineDustShow.configure(text = "초미세먼지 : "+indoorDataParse[4]+"㎍/㎍")
+	indoorHumidShow.configure(text = "습도 : " + indoorDataParse[5] + "%")
+	indoorTempShow.configure(text = "온도 : " + indoorDataParse[6] + "℃")
 	
 	if(int(indoorDataParse[1])<16):
 		indoorPM2Grade = 1
@@ -39,88 +40,166 @@ def updateData():
 		indoorPM10Grade = 3
 	else:
 		indoorPM10Grade = 4
-	
 	indoorDustTotalGrade = max(indoorPM2Grade,indoorPM10Grade)
 	print(indoorDustTotalGrade)
 
 	if(indoorDustTotalGrade==1):
 		indoorDustImg.img=PhotoImage(file = 'good.gif')
-		indoorDustImg.config(image = indoorDustImg.img)	
+		indoorDustImg.config(image = indoorDustImg.img, bg='#404040')	
 	elif(indoorDustTotalGrade==2):
 		indoorDustImg.img=PhotoImage(file = 'normal.gif')
-		indoorDustImg.config(image = indoorDustImg.img)	
+		indoorDustImg.config(image = indoorDustImg.img, bg='#404040')	
 	elif(indoorDustTotalGrade==3):
 		indoorDustImg.img=PhotoImage(file = 'bad.gif')
-		indoorDustImg.config(image = indoorDustImg.img)	
+		indoorDustImg.config(image = indoorDustImg.img,bg='#404040')	
 	elif(indoorDustToatalGrade==4):
 		indoorDustImg.img=PhotoImage(file = 'worst.gif')
-		indoorDustImg.config(image = indoorDustImg.img)	
+		indoorDustImg.config(image = indoorDustImg.img,bg='#404040')	
 	else:
 		indoorDustImg.img=PhotoImage(file = 'hmm.gif')
-		indoorDustImg.config(image = indoorDustImg.img)	
-	
+		indoorDustImg.config(image = indoorDustImg.img,bg='#404040')
 	outdoorDustShow.configure(text = outdoorDataRaw)
-	threading.Timer(1,updateData).start()
 	
 def showAll():
-	indoorDustImg.place(x=130,y=100)
-	indoorDustShow.place(x=50, y=200)
-	indoorTHShow.place(x=75, y=230)
-	outdoorDustImg.place(x=500,y=100)
-	outdoorDustShow.place(x=450, y=200)
-	outdoorTHShow.place(x=490, y=230)
+	indoorDustImg.place(x=220,y=210)
+	indoorDustShow.place(x=35, y=320)
+	indoorFineDustShow.place(x=35, y=350)
+	
+	indoorHumidImg.place(x=30,y=380)
+	indoorHumidShow.place(x=65, y=385)
+	
+	indoorTempImg.place(x=30,y=420)
+	indoorTempShow.place(x=65, y=425)
+	
+	outdoorDustImg.place(x=620,y=210)
+	outdoorDustShow.place(x=435, y=320)
+	outdoorFineDustShow.place(x=435,y=350)
+	
+	outdoorHumidImg.place(x=430,y=380)
+	outdoorHumidShow.place(x=465, y=385)
+	
+	outdoorTempImg.place(x=430,y=420)
+	outdoorTempShow.place(x=465, y=425)
 
 def showIndoor():
-	indoorDustImg.place(x=330,y=100)
-	indoorDustShow.place(x=250, y=200)
-	indoorTHShow.place(x=275, y=230)
+	indoorDustImg.place(x=360,y=200)
+	indoorDustShow.place(x=80, y=365)
+	indoorFineDustShow.place(x=80, y=425)
+	
+	indoorHumidImg.place(x=450,y=360)
+	indoorHumidShow.place(x=485, y=365)
+	
+	indoorTempImg.place(x=450,y=420)
+	indoorTempShow.place(x=485, y=425)
+	
 	outdoorDustImg.place_forget()
 	outdoorDustShow.place_forget()
-	outdoorTHShow.place_forget()
+	outdoorFineDustShow.place_forget()
+	
+	outdoorHumidImg.place_forget()
+	outdoorHumidShow.place_forget()
+	
+	outdoorTempImg.place_forget()
+	outdoorTempShow.place_forget()
 
 def showOutdoor():
 	indoorDustImg.place_forget()
 	indoorDustShow.place_forget()
-	indoorTHShow.place_forget()
-	outdoorDustImg.place(x=330,y=100)
-	outdoorDustShow.place(x=250, y=200)
-	outdoorTHShow.place(x=290, y=230)
+	indoorFineDustShow.place_forget()
 	
+	indoorHumidImg.place_forget()
+	indoorHumidShow.place_forget()
+	
+	indoorTempImg.place_forget()
+	indoorTempShow.place_forget()
+	
+	outdoorDustImg.place(x=360,y=200)
+	outdoorDustShow.place(x=80, y=365)
+	outdoorFineDustShow.place(x=80,y=425)
+	
+	outdoorHumidImg.place(x=450,y=360)
+	outdoorHumidShow.place(x=485, y=365)
+	
+	outdoorTempImg.place(x=450,y=420)
+	outdoorTempShow.place(x=485, y=425)
+
+def timeUpdate():
+	timeShow.configure(text = time.strftime('%Y/%m/%d %a %p %I:%M;%S',time.localtime(time.time())))
+	timeShow.pack(side="top",anchor="e")
+	updateData()
+	threading.Timer(1,timeUpdate).start()
 
 def noticeUpdate():
 	noticeShow.pack(side="top", fill="x")
 	
 win.title("My First GUI")
 win.geometry("800x480")
+win.attributes("-fullscreen",True)
+win.configure(bg='#404040')
 
-showAllButton = Button(win, text="모두 보기", command = showAll,height = 1, width = 10)
-showAllButton.place(x=5, y=400)
+showAllButtonImg = PhotoImage(file='all.gif')
+showAllButton = Button(win, image=showAllButtonImg, command = showAll, bg='#404040', highlightthickness=0, bd=0)
+showAllButton.place(x=120, y=70)
 
-showIndoorButton = Button(win, text="실내 보기", command = showIndoor,height = 1, width = 10)
-showIndoorButton.place(x=110, y=400)
+showIndoorButtonImg = PhotoImage(file='indoor.gif')
+showIndoorButton = Button(win, image=showIndoorButtonImg, command = showIndoor, bg='#404040', highlightthickness=0, bd=0)
+showIndoorButton.place(x=360, y=70)
 
-showIndoorButton = Button(win, text="실외 보기", command = showOutdoor,height = 1, width = 10)
-showIndoorButton.place(x=215, y=400)
+showOutdoorButtonImg = PhotoImage(file='outdoor.gif')
+showOutdoorButton = Button(win, image=showOutdoorButtonImg, command = showOutdoor, bg='#404040', highlightthickness=0, bd=0)
+showOutdoorButton.place(x=600, y=70)
 
-exitButton = Button(win, text="종료",command=exitProgram,height=1,width=10)
-exitButton.place(x=690, y = 400)
+exitButton = Button(win, text="종료",command=exitProgram,height=1,width=5, bg='#404040', fg='#FFFFFF', highlightthickness=0)
+exitButton.place(x=720, y = 440)
 
-indoorDustShow = Label(win, text='Outdoor Dust Data Here')
+indoorDustShow = Label(win, text='내부 미세먼지 정보')
+indoorDustShow.configure(bg='#404040',fg='#FFFFFF')
+indoorFineDustShow = Label(win, text='내부 초미세먼지 정보')
+indoorFineDustShow.configure(bg='#404040',fg='#FFFFFF')
 indoorDustImg = Label(win)
 indoorDustImg.img=PhotoImage(file = 'hmm.gif')
 indoorDustImg.config(image = indoorDustImg.img)
 
-outdoorDustShow = Label(win, text='Outdoor Dust Data Here')
+outdoorDustShow = Label(win, text='외부 미세먼지 정보')
+outdoorDustShow.configure(bg='#404040',fg='#FFFFFF')
+outdoorFineDustShow = Label(win, text='외부 초미세먼지 정보')
+outdoorFineDustShow.configure(bg='#404040',fg='#FFFFFF')
 outdoorDustImg = Label(win)
 outdoorDustImg.img=PhotoImage(file = 'hmm.gif')
 outdoorDustImg.config(image = outdoorDustImg.img)
 
-indoorTHShow = Label(win, text='Indoor Humid Temp & Data here')
-outdoorTHShow = Label(win, text='Outdoor Humid & Temp Here')
-noticeShow = Label(win, text = 'Noooooooootice Shooooooows Heeeeeeere!')
+indoorHumidImg = Label(win)
+indoorHumidImg.img = PhotoImage(file='humid.gif')
+indoorHumidImg.config(image=indoorHumidImg.img, bg='#404040')
+indoorHumidShow = Label(win, text='내부 습도')
+indoorHumidShow.configure(bg='#404040', fg='#FFFFFF')
+
+indoorTempImg = Label(win)
+indoorTempImg.img = PhotoImage(file='temp.gif')
+indoorTempImg.config(image=indoorTempImg.img, bg='#404040')
+indoorTempShow = Label(win, text='내부 온도')
+indoorTempShow.configure(bg='#404040', fg='#FFFFFF')
+
+outdoorHumidImg = Label(win)
+outdoorHumidImg.img = PhotoImage(file='humid.gif')
+outdoorHumidImg.config(image=outdoorHumidImg.img, bg='#404040')
+outdoorHumidShow = Label(win, text='외부 습도')
+outdoorHumidShow.configure(bg='#404040', fg='#FFFFFF')
+
+outdoorTempImg = Label(win)
+outdoorTempImg.img = PhotoImage(file='temp.gif')
+outdoorTempImg.config(image=outdoorTempImg.img, bg='#404040')
+outdoorTempShow = Label(win, text='외부 온도')
+outdoorTempShow.configure(bg='#404040', fg='#FFFFFF')
+
+timeShow = Label(win, text = '날짜 및 시간')
+timeShow.configure(bg='#404040', fg='#FFFFFF')
+noticeShow = Label(win, text = '공지사항은 여기에 표시됩니다!')
+noticeShow.configure(bg='#404040', fg='#FFFFFF')
 
 updateData()
 showAll()
+timeUpdate()
 noticeUpdate()
 
 mainloop()
